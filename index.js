4146b6f3d1a9d5ca877f346115c87e92c365932f
+ <+>
2023-09-19 15:20:39 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+const square = x => {
+x=2;
+return x*x;
+}
+
+const add =(a,b) => {
+a=3;
+b=4;
+return a+b;
+}
+ const divide =function () {
+    let number1=2000;
+    let number2=100;
+    return number1/number2;
+ }
